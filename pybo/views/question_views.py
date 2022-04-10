da61d7e3154d91a959bbb75571eedd79bfc69111
lee seung kyu <lsk920110@gmail.com>
2022-04-10 23:05:27 +0900

    글쓰기 페이지 요청 관련 수정 디테일 부트스트렙 적용

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
-from pybo.forms import QuestionForm
+from flask import Blueprint, render_template, request, url_for
+from pybo.forms import QuestionForm , AnswerForm
 
 from pybo.models import Question
+
+from datetime import datetime
+from werkzeug.utils import redirect
+from .. import db
 
 bp = Blueprint('question',__name__,url_prefix='/')
 
@@ -12,10 +16,21 @@ def _list():
             #render_template(이동할페이지,전송할 데이터의 이름과,데이터)
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html',question=question)
+    return render_template('question/question_detail.html',question=question, form=form)
 
-@bp.route('/create')
+@bp.route('/create', methods = ('GET','POST'))
 def create():
     form = QuestionForm()
+
+    #r구분을 method와 변수의 유효성 검사로 한다...
+    if request.method == 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+
+
+
     return render_template('question/question_form.html',form=form)
